0e1f0f907ac5a37f61ed4a7f5fc3aea94487632a
Kevin Smith <KevinH.Smith@coxinc.com>
2016-08-26 11:21:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# remock
+
+Module mocking for unit testing
+
+## Install
+
+```sh
+npm install remock
+```
---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = remock;
+
+function remock() {
+  throw new Error('Coming soon to a machine near you');
+}
